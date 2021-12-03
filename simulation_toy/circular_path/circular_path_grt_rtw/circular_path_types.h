3c9e655950da68cee41fad29ad1ed45163ce857a
SimonZhan-code <simonzhan@berkeley.edu>
2021-12-03 10:46:06 -0800

    code generation

---
+++ simulation_toy/circular_path/circular_path_grt_rtw/circular_path_types.h
@@ -0,0 +1,35 @@
+/*
+ * circular_path_types.h
+ *
+ * Academic License - for use in teaching, academic research, and meeting
+ * course requirements at degree granting institutions only.  Not for
+ * government, commercial, or other organizational use.
+ *
+ * Code generation for model "circular_path".
+ *
+ * Model version              : 1.2
+ * Simulink Coder version : 9.6 (R2021b) 14-May-2021
+ * C source code generated on : Fri Dec  3 10:37:35 2021
+ *
+ * Target selection: grt.tlc
+ * Note: GRT includes extra infrastructure and instrumentation for prototyping
+ * Embedded hardware selection: Intel->x86-64 (Windows64)
+ * Code generation objectives: Unspecified
+ * Validation result: Not run
+ */
+
+#ifndef RTW_HEADER_circular_path_types_h_
+#define RTW_HEADER_circular_path_types_h_
+#include "rtwtypes.h"
+#include "builtin_typeid_types.h"
+#include "multiword_types.h"
+
+/* Model Code Variants */
+
+/* Parameters (default storage) */
+typedef struct P_circular_path_T_ P_circular_path_T;
+
+/* Forward declaration for rtModel */
+typedef struct tag_RTM_circular_path_T RT_MODEL_circular_path_T;
+
+#endif                                 /* RTW_HEADER_circular_path_types_h_ */
