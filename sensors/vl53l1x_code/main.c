42ce2b80e12799a81e291159c819e679c976f638
Victor_Li <lytz@berkeley.edu>
2021-12-04 22:31:47 -0800

    try control kobuki

--- sensors/vl53l1x_code/main.c
+++ sensors/vl53l1x_code/main.c
@@ -8,6 +8,11 @@
 #include "nrf_power.h"
 #include "buckler.h"
 #include "display.h"
+#include "kobukiActuator.h"
+#include "kobukiUtilities.h"
+#include "kobukiSensorPoll.h"
+#include "kobukiSensorTypes.h"
+#include "mpu9250.h"
 
 // Pin configurations
 #define LED NRF_GPIO_PIN_MAP(0, 17)
@@ -21,6 +26,8 @@ uint8_t *data_array = (uint8_t *)&data;
 uint32_t r_error = 0;
 int i = 0;
 
+// I2C manager
+NRF_TWI_MNGR_DEF(twi_mngr_instance, 5, 0);
 
 // error handler for UART
 void uart_error_handle(app_uart_evt_t *p_event)
@@ -35,6 +42,9 @@ void uart_error_handle(app_uart_evt_t *p_event)
             } else {
                 kobukiDriveDirect(0, 0);
             }
+            char buf [32];
+            snprintf(buf, 32, "%f", data);
+            display_write (buf, DISPLAY_LINE_1);
             i = 0;
             data = 0;
         }
@@ -96,12 +106,46 @@ int main(void)
     // init uart
     uart_init();
 
-    char buf [16];
-    snprintf(buf, 16, "%f", data);
-    display_write (buf, DISPLAY_LINE_1);
+    // initialize RTT library
+    error_code = NRF_LOG_INIT(NULL);
+    APP_ERROR_CHECK(error_code);
+    NRF_LOG_DEFAULT_BACKENDS_INIT();
+    printf("Log initialized!\n");
+
+    // initialize i2c master (two wire interface)
+    nrf_drv_twi_config_t i2c_config = NRF_DRV_TWI_DEFAULT_CONFIG;
+    i2c_config.scl = BUCKLER_SENSORS_SCL;
+    i2c_config.sda = BUCKLER_SENSORS_SDA;
+    i2c_config.frequency = NRF_TWIM_FREQ_100K;
+    error_code = nrf_twi_mngr_init(&twi_mngr_instance, &i2c_config);
+    APP_ERROR_CHECK(error_code);
+    mpu9250_init(&twi_mngr_instance);
+    printf("IMU initialized!\n");
+
+    // initialize display
+    nrf_drv_spi_t spi_instance = NRF_DRV_SPI_INSTANCE(1);
+    nrf_drv_spi_config_t spi_config = {
+        .sck_pin = BUCKLER_LCD_SCLK,
+        .mosi_pin = BUCKLER_LCD_MOSI,
+        .miso_pin = BUCKLER_LCD_MISO,
+        .ss_pin = BUCKLER_LCD_CS,
+        .irq_priority = NRFX_SPI_DEFAULT_CONFIG_IRQ_PRIORITY,
+        .orc = 0,
+        .frequency = NRF_DRV_SPI_FREQ_4M,
+        .mode = NRF_DRV_SPI_MODE_2,
+        .bit_order = NRF_DRV_SPI_BIT_ORDER_MSB_FIRST
+    };
+    error_code = nrf_drv_spi_init(&spi_instance, &spi_config, NULL, NULL);
+    APP_ERROR_CHECK(error_code);
+    display_init(&spi_instance);
+
+    // initialize kobuki
+    kobukiInit();
+    KobukiSensors_t sensors = {0};
 
     while (1)
     {
+        //kobukiSensorPoll(&sensors);
         nrf_gpio_pin_toggle(LED);
         nrf_delay_ms(1);
     }
