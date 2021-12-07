94d1a924be5489680364133636ce73c8aebd7ba6
Will Wang <will.wang@berkeley.edu>
2021-12-06 19:48:17 -0800

    Successfully switch UART channel between Kobuki and distance sensor to read outputs and send commands.

--- sensors/tsl2561_code/main.c
+++ sensors/tsl2561_code/main.c
@@ -11,12 +11,12 @@
 #include "app_timer.h"
 #include "nrf.h"
 #include "nrf_delay.h"
+#include "nrf_drv_spi.h"
 #include "nrf_gpio.h"
 #include "nrf_log.h"
 #include "nrf_log_ctrl.h"
 #include "nrf_log_default_backends.h"
 #include "nrf_pwr_mgmt.h"
-#include "nrf_drv_spi.h"
 
 #include "buckler.h"
 #include "display.h"
@@ -60,16 +60,15 @@ int main(void) {
   // initialize display
   nrf_drv_spi_t spi_instance = NRF_DRV_SPI_INSTANCE(1);
   nrf_drv_spi_config_t spi_config = {
-    .sck_pin = BUCKLER_LCD_SCLK,
-    .mosi_pin = BUCKLER_LCD_MOSI,
-    .miso_pin = BUCKLER_LCD_MISO,
-    .ss_pin = BUCKLER_LCD_CS,
-    .irq_priority = NRFX_SPI_DEFAULT_CONFIG_IRQ_PRIORITY,
-    .orc = 0,
-    .frequency = NRF_DRV_SPI_FREQ_4M,
-    .mode = NRF_DRV_SPI_MODE_2,
-    .bit_order = NRF_DRV_SPI_BIT_ORDER_MSB_FIRST
-  };
+      .sck_pin = BUCKLER_LCD_SCLK,
+      .mosi_pin = BUCKLER_LCD_MOSI,
+      .miso_pin = BUCKLER_LCD_MISO,
+      .ss_pin = BUCKLER_LCD_CS,
+      .irq_priority = NRFX_SPI_DEFAULT_CONFIG_IRQ_PRIORITY,
+      .orc = 0,
+      .frequency = NRF_DRV_SPI_FREQ_4M,
+      .mode = NRF_DRV_SPI_MODE_2,
+      .bit_order = NRF_DRV_SPI_BIT_ORDER_MSB_FIRST};
   error_code = nrf_drv_spi_init(&spi_instance, &spi_config, NULL, NULL);
   APP_ERROR_CHECK(error_code);
   display_init(&spi_instance);
@@ -96,7 +95,7 @@ int main(void) {
   robot_state_t state = OFF;
   KobukiSensors_t sensors = {0};
   float angle = 90;
-  char buf[16]; 
+  char buf[16];
   static uint8_t lux_buf = 3;
   uint16_t lux_left[lux_buf];
   uint16_t lux_right[lux_buf];
@@ -105,7 +104,6 @@ int main(void) {
   uint32_t left_avg = 500;
   uint32_t right_avg = 500;
   uint32_t diff_max = 500;
-  
 
   // loop forever, running state machine
   while (1) {
@@ -115,7 +113,7 @@ int main(void) {
     // lux_right[lux_count] = tsl2561_read_result(TSL2561_ADDR_HIGH);
     left_avg = tsl2561_read_result(TSL2561_ADDR_FLOAT);
     right_avg = tsl2561_read_result(TSL2561_ADDR_HIGH);
-    lux_count = (lux_count + 1) / lux_buf; 
+    lux_count = (lux_count + 1) / lux_buf;
 
     // delay before continuing
     // Note: removing this delay will make responses quicker, but will result
@@ -123,69 +121,67 @@ int main(void) {
     nrf_delay_ms(1);
 
     // handle states
-    switch(state) {
-      case OFF: {
-        // transition logic
-        if (is_button_pressed(&sensors)) {
-          state = DRIVING;
-          // kobukiDriveDirect(60 + lux_high / 300, 60 + lux_float / 300);
-        } else {
-          // left_avg = mean(lux_buf, lux_left);
-          // right_avg = mean(lux_buf, lux_right);
-          // perform state-specific actions here
-          snprintf(buf, 16, "L=%d, R=%d", left_avg, right_avg);
-          printf("%s\n", buf);
-          display_write(buf, DISPLAY_LINE_0);
-          angle = (left_avg - right_avg) / 12.5;
-          snprintf(buf, 16, "Angle=%f\n", angle);
-          display_write(buf, DISPLAY_LINE_1);
-          printf(buf);
+    switch (state) {
+    case OFF: {
+      // transition logic
+      if (is_button_pressed(&sensors)) {
+        state = DRIVING;
+        // kobukiDriveDirect(60 + lux_high / 300, 60 + lux_float / 300);
+      } else {
+        // left_avg = mean(lux_buf, lux_left);
+        // right_avg = mean(lux_buf, lux_right);
+        // perform state-specific actions here
+        snprintf(buf, 16, "L=%d, R=%d", left_avg, right_avg);
+        printf("%s\n", buf);
+        display_write(buf, DISPLAY_LINE_0);
+        angle = (left_avg - right_avg) / 12.5;
+        snprintf(buf, 16, "Angle=%f\n", angle);
+        display_write(buf, DISPLAY_LINE_1);
+        printf(buf);
 
-          state = OFF;
-        }
-        break; // each case needs to end with break!
+        state = OFF;
+      }
+      break; // each case needs to end with break!
+    }
+
+    case INIT: {
+      if (init_time < lux_buf) {
+        // wait for lux to populate
+        snprintf(buf, 16, "Initiating...%d", init_time);
+        display_write(buf, DISPLAY_LINE_0);
+        init_time++;
+      } else {
+        left_avg = mean(lux_buf, lux_left);
+        right_avg = mean(lux_buf, lux_right);
+        state = OFF;
       }
 
-      case INIT: {
-        if (init_time < lux_buf) {
-          // wait for lux to populate
-          snprintf(buf, 16, "Initiating...%d", init_time);
-          display_write(buf, DISPLAY_LINE_0);
-          init_time++;
-        } else {
-          left_avg = mean(lux_buf, lux_left);
-          right_avg = mean(lux_buf, lux_right);
-          state = OFF;
-        }
+      break;
+    }
 
-        break;
+    case DRIVING: {
+      // transition logic
+      if (is_button_pressed(&sensors)) {
+        state = OFF;
+        kobukiDriveDirect(0, 0);
+      } else {
+        // perform state-specific actions here
+        left_avg = mean(lux_buf, lux_left);
+        right_avg = mean(lux_buf, lux_right);
+        // kobukiDriveDirect(60 + lux_high / 300, 60 + lux_float / 300);
+        snprintf(buf, 16, "L=%d, R=%d", left_avg, right_avg);
+        printf("%s\n", buf);
+        display_write(buf, DISPLAY_LINE_0);
+        angle = 90 - (left_avg - right_avg) / 500 * 90;
+        snprintf(buf, 16, "Angle=%f", angle);
+        display_write(buf, DISPLAY_LINE_1);
+
+        state = DRIVING;
       }
-
-      case DRIVING: {
-        // transition logic
-        if (is_button_pressed(&sensors)) {
-          state = OFF;
-          kobukiDriveDirect(0, 0);
-        } else {
-          // perform state-specific actions here
-          left_avg = mean(lux_buf, lux_left);
-          right_avg = mean(lux_buf, lux_right);
-          // kobukiDriveDirect(60 + lux_high / 300, 60 + lux_float / 300);
-          snprintf(buf, 16, "L=%d, R=%d", left_avg, right_avg);
-          printf("%s\n", buf);
-          display_write(buf, DISPLAY_LINE_0);
-          angle = 90 - (left_avg - right_avg) / 500 * 90;
-          snprintf(buf, 16, "Angle=%f", angle);
-          display_write(buf, DISPLAY_LINE_1);
-
-          state = DRIVING;
-        }
-        break; // each case needs to end with break!
-      }
+      break; // each case needs to end with break!
+    }
 
       // add other cases here
-
     }
   }
 }
-
--- sensors/vl53l1x_code/main.c
+++ sensors/vl53l1x_code/main.c
@@ -2,30 +2,30 @@
 #include <stdint.h>
 #include <stdio.h>
 
-#include "nrf_gpio.h"
+#include "app_uart.h"
 #include "nrf_delay.h"
-#include "nrf_uarte.h"
-#include "nrf_power.h"
+#include "nrf_gpio.h"
 #include "nrf_log.h"
 #include "nrf_log_ctrl.h"
 #include "nrf_log_default_backends.h"
-
-#include "app_uart.h"
+#include "nrf_power.h"
+#include "nrf_uarte.h"
 
 #include "buckler.h"
 #include "display.h"
 #include "kobukiActuator.h"
-#include "kobukiUtilities.h"
 #include "kobukiSensorPoll.h"
 #include "kobukiSensorTypes.h"
+#include "kobukiUtilities.h"
 #include "mpu9250.h"
 
 // Pin configurations
-#define UART_RX NRF_GPIO_PIN_MAP(0, 8)
-#define UART_TX NRF_GPIO_PIN_MAP(0, 6)
-#define UART_TX_BUF_SIZE 256
-#define UART_RX_BUF_SIZE 256
+#define UART_RX NRF_GPIO_PIN_MAP(0, 15)
+#define UART_TX NRF_GPIO_PIN_MAP(0, 16)
+#define UART_TX_BUF_SIZE 128
+#define UART_RX_BUF_SIZE 128
 
+// Variables for reading distance sensor output
 float data;
 uint8_t *data_array = (uint8_t *)&data;
 uint32_t r_error = 0;
@@ -34,123 +34,117 @@ int i = 0;
 // I2C manager
 // NRF_TWI_MNGR_DEF(twi_mngr_instance, 5, 0);
 
-// error handler for UART
-void uart_error_handle(app_uart_evt_t *p_event)
-{
-    if (p_event->evt_type == APP_UART_DATA)
-    {
-        if (i == 4)
-        {
-            printf("Reading: %f\n", data);
-            // if (data < 2)
-            // {
-            //     kobukiDriveDirect(20, 20);
-            // }
-            // else
-            // {
-            //     kobukiDriveDirect(0, 0);
-            // }
-            // char buf[32];
-            // snprintf(buf, 32, "%f", data);
-            // display_write(buf, DISPLAY_LINE_1);
-            i = 0;
-            data = 0;
-        }
-        uint8_t r_data = 0;
-        r_error = app_uart_get(&r_data);
-        if (r_error == NRF_SUCCESS)
-        {
-            data_array[i] = r_data;
-        }
-        else
-        {
-            printf("Reading error!");
-        }
-        i++;
+// Event handler for UART
+void uart_event_handle(app_uart_evt_t *p_event) {
+  if (p_event->evt_type == APP_UART_DATA) {
+    uint8_t r_data = 0;
+    r_error = app_uart_get(&r_data);
+    if (r_error == NRF_SUCCESS && i < 4) {
+      data_array[i] = r_data;
+      printf("%d\n", i);
+    } else {
+      printf("Reading ends!");
     }
-    // just call app error handler
-    else if (p_event->evt_type == APP_UART_COMMUNICATION_ERROR)
-    {
-        //APP_ERROR_HANDLER(p_event->data.error_communication);
-    }
-    else if (p_event->evt_type == APP_UART_FIFO_ERROR)
-    {
-        APP_ERROR_HANDLER(p_event->data.error_code);
-    }
+    i++;
+  } else if (p_event->evt_type == APP_UART_COMMUNICATION_ERROR) {
+    // commented out because of initializing errors
+    // APP_ERROR_HANDLER(p_event->data.error_communication);
+  } else if (p_event->evt_type == APP_UART_FIFO_ERROR) {
+    APP_ERROR_HANDLER(p_event->data.error_code);
+  }
 }
 
 // initialization of UART
-void uart_init(void)
-{
-    uint32_t err_code;
+void uart_init(void) {
+  uint32_t err_code;
 
-    // configure RX and TX pins
-    // no RTS or CTS pins with flow control disabled
-    // no parity
-    // baudrate 115200
-    const app_uart_comm_params_t comm_params = {
-        UART_RX,
-        UART_TX,
-        0,
-        0,
-        APP_UART_FLOW_CONTROL_DISABLED,
-        false,
-        NRF_UARTE_BAUDRATE_115200};
+  // configure RX and TX pins
+  // no RTS or CTS pins with flow control disabled
+  // no parity
+  // baudrate 115200
+  const app_uart_comm_params_t comm_params = {
+      UART_RX,
+      UART_TX,
+      0,
+      0,
+      APP_UART_FLOW_CONTROL_DISABLED,
+      false,
+      NRF_UARTE_BAUDRATE_115200};
 
-    // actually initialize UART
-    APP_UART_FIFO_INIT(&comm_params, UART_RX_BUF_SIZE, UART_TX_BUF_SIZE,
-                       uart_error_handle, APP_IRQ_PRIORITY_LOW, err_code);
-    APP_ERROR_CHECK(err_code);
-
-    printf("ERROR: %d\n", err_code);
+  // actually initialize UART
+  APP_UART_FIFO_INIT(&comm_params, UART_RX_BUF_SIZE, UART_TX_BUF_SIZE,
+                     uart_error_handle, APP_IRQ_PRIORITY_LOW, err_code);
+  APP_ERROR_CHECK(err_code);
 }
 
-int main(void)
-{
-    // initialize RTT library
-    ret_code_t error_code = NRF_LOG_INIT(NULL);
-    APP_ERROR_CHECK(error_code);
-    NRF_LOG_DEFAULT_BACKENDS_INIT();
-    printf("Log initialized!\n");
+int main(void) {
 
-    // initialize i2c master (two wire interface)
-    // nrf_drv_twi_config_t i2c_config = NRF_DRV_TWI_DEFAULT_CONFIG;
-    // i2c_config.scl = BUCKLER_SENSORS_SCL;
-    // i2c_config.sda = BUCKLER_SENSORS_SDA;
-    // i2c_config.frequency = NRF_TWIM_FREQ_100K;
-    // error_code = nrf_twi_mngr_init(&twi_mngr_instance, &i2c_config);
-    // APP_ERROR_CHECK(error_code);
-    // mpu9250_init(&twi_mngr_instance);
-    // printf("IMU initialized!\n");
+  // initialize RTT library
+  ret_code_t error_code = NRF_LOG_INIT(NULL);
+  APP_ERROR_CHECK(error_code);
+  NRF_LOG_DEFAULT_BACKENDS_INIT();
+  printf("Log initialized!\n");
 
-    // initialize display
-    // nrf_drv_spi_t spi_instance = NRF_DRV_SPI_INSTANCE(1);
-    // nrf_drv_spi_config_t spi_config = {
-    //     .sck_pin = BUCKLER_LCD_SCLK,
-    //     .mosi_pin = BUCKLER_LCD_MOSI,
-    //     .miso_pin = BUCKLER_LCD_MISO,
-    //     .ss_pin = BUCKLER_LCD_CS,
-    //     .irq_priority = NRFX_SPI_DEFAULT_CONFIG_IRQ_PRIORITY,
-    //     .orc = 0,
-    //     .frequency = NRF_DRV_SPI_FREQ_4M,
-    //     .mode = NRF_DRV_SPI_MODE_2,
-    //     .bit_order = NRF_DRV_SPI_BIT_ORDER_MSB_FIRST};
-    // error_code = nrf_drv_spi_init(&spi_instance, &spi_config, NULL, NULL);
-    // APP_ERROR_CHECK(error_code);
-    // display_init(&spi_instance);
-    // printf("Display initialized!\n");
+  // initialize i2c master (two wire interface)
+  // nrf_drv_twi_config_t i2c_config = NRF_DRV_TWI_DEFAULT_CONFIG;
+  // i2c_config.scl = BUCKLER_SENSORS_SCL;
+  // i2c_config.sda = BUCKLER_SENSORS_SDA;
+  // i2c_config.frequency = NRF_TWIM_FREQ_100K;
+  // error_code = nrf_twi_mngr_init(&twi_mngr_instance, &i2c_config);
+  // APP_ERROR_CHECK(error_code);
+  // mpu9250_init(&twi_mngr_instance);
+  // printf("IMU initialized!\n");
 
-    // initialize kobuki
-    // kobukiInit();
-    // KobukiSensors_t sensors = {0};
-    // printf("Kobuki initialized!\n");
+  // initialize display
+  // nrf_drv_spi_t spi_instance = NRF_DRV_SPI_INSTANCE(1);
+  // nrf_drv_spi_config_t spi_config = {
+  //     .sck_pin = BUCKLER_LCD_SCLK,
+  //     .mosi_pin = BUCKLER_LCD_MOSI,
+  //     .miso_pin = BUCKLER_LCD_MISO,
+  //     .ss_pin = BUCKLER_LCD_CS,
+  //     .irq_priority = NRFX_SPI_DEFAULT_CONFIG_IRQ_PRIORITY,
+  //     .orc = 0,
+  //     .frequency = NRF_DRV_SPI_FREQ_4M,
+  //     .mode = NRF_DRV_SPI_MODE_2,
+  //     .bit_order = NRF_DRV_SPI_BIT_ORDER_MSB_FIRST};
+  // error_code = nrf_drv_spi_init(&spi_instance, &spi_config, NULL, NULL);
+  // APP_ERROR_CHECK(error_code);
+  // display_init(&spi_instance);
+  // printf("Display initialized!\n");
 
-    // init uart
+  // initialize kobuki
+  kobukiInit();
+  KobukiSensors_t sensors = {0};
+  printf("Kobuki initialized!\n");
+
+  while (1) {
+    // kobukiSensorPoll(&sensors);
+
+    // un-init Kobuki
+    kobukiUARTUnInit();
+
+    // init uart for distance sensor, then un-init
+    printf("***************************************************************\n");
     uart_init();
-
-    while (1)
-    {
-        //kobukiSensorPoll(&sensors);
-        nrf_delay_ms(1);
+    while (i < 4) {
+      nrf_delay_ms(1);
     }
+    app_uart_close();
+    printf("***************************************************************\n");
+
+    // re-init uart for Kobuki and send commands
+    kobukiUARTInit();
+    printf("Reading: %f\n", data);
+    if (data < 2) {
+      printf("GOOOOOOOO\n");
+      kobukiDriveDirect(100, 100);
+    } else {
+      kobukiDriveDirect(0, 0);
+    }
+    i = 0;
+    data = 0;
+
+    // delay some time between each loop for correct behaviors
+    nrf_delay_ms(40);
+  }
 }
